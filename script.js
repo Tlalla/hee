d53bbd276aa48934694ff65067add72f58770243
Tlalla <taqievalala@gmail.com>
2024-11-14 20:32:18 +0400

    sukur bir sey eliye bildik

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 let add=document.querySelector('.add');
-let close =document.querySelector('.delete');
+let plus=document.querySelector('.plus');
+let close=document.querySelector('.close');
 let alphabet=document.querySelector('.alphabet');
 let input=document.querySelector('input');
+let zablack=document.querySelector('.zablack');
+let zagray=document.querySelector('.zagray');
+let azblack=document.querySelector('.azblack')
+close.addEventListener('mouseover',()=>{
+    close.src ='./pictures/purplex.svg';
+});
+close.addEventListener('mouseout',()=>{
+    close.src='./pictures/deletebtn.svg';
+});
+alphabet.addEventListener('mouseenter',()=>{
+    alphabet.classList.add('none');
+    azblack.classList.remove('none');
+    zagray.classList.add('none');
+})
+alphabet.addEventListener('mouseleave',()=>{
+    alphabet.classList.remove('none');
+    azblack.classList.add('none');
+})
+zagray.addEventListener('mouseover',()=>{
+    zagray.classList.add('none');
+    zablack.classList.remove('none');
+})
+zablack.addEventListener('mouseout',()=>{
+    zagray.classList.remove('none');
+    zablack.classList.add('none');
+})
+azblack.addEventListener('click',()=>{
+    alphabet.classList.add('none')
+    azblack.classList.add('none');
+    zablack.classList.remove('none');
+})
+zablack.addEventListener('click',()=>{
+    zablack.classList.add('none');
+    alphabet.classList.remove('none');
+})
+
+add.addEventListener('click',()=>{
+    
+})
