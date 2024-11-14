7f7a5373d88a98c1c62359b1703835c4c9b07478
Tlalla <taqievalala@gmail.com>
2024-11-15 00:40:25 +0400

    gel ey seheer

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ let input=document.querySelector('input');
 let zablack=document.querySelector('.zablack');
 let zagray=document.querySelector('.zagray');
 let azblack=document.querySelector('.azblack')
+let list=document.querySelector('.list')
+let olList=document.querySelector('ol');
+let inp=document.querySelector('.inp');
 close.addEventListener('mouseover',()=>{
     close.src ='./pictures/purplex.svg';
 });
 close.addEventListener('mouseout',()=>{
     close.src='./pictures/deletebtn.svg';
 });
+close.addEventListener('click',()=>{
+    input.value='';
+})
 alphabet.addEventListener('mouseenter',()=>{
     alphabet.classList.add('none');
     azblack.classList.remove('none');
@@ -40,5 +46,37 @@ zablack.addEventListener('click',()=>{
 })
 
 add.addEventListener('click',()=>{
-    
+    let inpvalue=input.value.trim();
+    if (inpvalue!=''){
+        list.classList.remove('none')
+        let items=document.createElement('li');
+        items.className='item';
+        items.textContent=inpvalue;
+        olList.appendChild(items);
+        let closebutton=document.createElement('img');
+        closebutton.className='cls ';
+        closebutton.src='./pictures/deletebtn.svg';
+        items.append(closebutton);
+
+        // items.insertAdjacentHTML('afterend', '<img src="./pictures/deletebtn.svg" class="closebtn" alt="Close/Delete">');
+        //     let imgXGray = listItem.nextSibling;
+    closebutton.addEventListener('click',()=>{
+    olList.removeChild(items);
+    if(olList.children.length===0){
+        list.classList.add('none');
+    }
+});
+closebutton.addEventListener('mouseover',()=>{
+    closebutton.src ='./pictures/purplex.svg';
+});
+closebutton.addEventListener('mouseout',()=>{
+    closebutton.src='./pictures/deletebtn.svg';
+});
+
+input.value='';
+inp.classList.add('none');
+}
+});
+plus.addEventListener('click',()=>{
+    inp.classList.remove('none');
 })
